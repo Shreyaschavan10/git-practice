5ae241f41e182c4c481e88bfb9c8f99120d08f33
Shreyaschavan10 <chavanshreyas10@gmail.com>
2025-02-14 17:06:46 +0530

    files added

---
+++ pract.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Employees (
+    EmployeeID INT AUTO_INCREMENT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Age INT,
+    Salary DECIMAL(10,2),
+    Department VARCHAR(50),
+    HireDate DATE
+);
